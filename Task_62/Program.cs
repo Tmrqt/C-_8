be6aec6854d6fc41d7f5c47fb27904bdbe101132
Timerkhan <timerkhan12@gmail.com>
2022-12-29 18:38:53 +0500

    done

---
+++ Task_62/Program.cs
@@ -0,0 +1,50 @@
+﻿int n = 4;
+
+int[,] NewArr (int n)
+{
+    int[,] arr = new int[n,n];
+    int counter = 1;
+    for (int itr = 0; itr < 2; itr++)
+    {
+        for (int j = 0 + itr; j < n - itr; j++) 
+        {    
+            arr[0+itr,j] = counter;
+            counter++;
+        }
+        counter--;
+        for (int i = 0 + itr; i < n - itr; i++)
+        { 
+            arr[i,n-1-itr] = counter;
+            counter++;
+        }
+        counter--;
+        for (int j = n-1-itr; j >= 0 + itr; j--) 
+        {    
+            arr[n-1 - itr,j] = counter;
+            counter++;
+        }
+        counter--;
+        for (int i = n-1-itr; i >= 1 + itr; i--)
+        {
+            arr[i,0+itr] = counter;
+            counter++;
+        }
+    }
+    return arr;
+}
+
+int[,] ShowArr (int[,] arr)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            System.Console.Write(arr[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    return arr;
+}
+
+int[,] arr = NewArr(n);
+ShowArr(arr);
